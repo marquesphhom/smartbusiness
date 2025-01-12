4e40eb910112148fa83651af0cdc2ac75dc0f219
Pedro Marques <pedro.marques.hudson@gmail.com>
2025-01-12 20:24:42 -0300

    Fourth Commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const sidebar = document.querySelector('.sidebar');
 const left = document.querySelector('.left');
-
+const right = document.querySelector('.right');
 
 left.addEventListener('click', () => {
     sidebar.classList.toggle('active');
@@ -9,3 +9,22 @@ left.addEventListener('click', () => {
 sidebar.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 })
+
+function getDate(element){
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+    let time = `${day}/${month+1}/${year} ${hour}:${minute}:${second}`;
+    const h1 = document.createElement('h1');
+    h1.textContent = time;
+    element.appendChild(h1);
+}
+
+let myInterval = setInterval(() => {
+    right.innerHTML = '';
+    getDate(right)
+}, 1000)
